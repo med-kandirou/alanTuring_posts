0c9a548f58f66790519b355d132bc89613ed84d3
kandirou0 <simo.kandirou15@gmail.com>
2023-01-18 15:07:20 +0100

    update

--- Front-end/src/views/guest/inscrire.vue
+++ Front-end/src/views/guest/inscrire.vue
@@ -11,14 +11,14 @@
                     </h1>
                     <form class="space-y-4 md:space-y-6">
                         <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                 Nom</label>
                             <input type="text" v-model="nom"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="name@company.com">
                         </div>
                         <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                 email</label>
                             <input type="email" name="email" v-model="email"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -112,7 +112,6 @@ export default {
                         this.$router.push('/login')
                         }   
                     )
-                
             }
         }
     }
